f218de920291be121cf3944ad90fab262afc0c7f
Younes51-1 <77343582+Younes51-1@users.noreply.github.com>
2021-09-28 11:55:03 -0400

    temp

--- exercice.py
+++ exercice.py
@@ -2,46 +2,80 @@
 # -*- coding: utf-8 -*-
 
 
-def order(values: list = None) -> list:
-    if values is None:
-        # TODO: demander les valeurs ici
-        pass
+from typing import List
 
-    return []
+
+def order(values: list = None) -> list:
+    values=list([])
+    for i in range(10) :
+        entrez=input("Entrez une valeur : ")
+        if entrez.isdigit()==True :
+            values.append(float(entrez))
+        else:
+            values.append(entrez)
+        values.sort()
+    return values
 
 
 def anagrams(words: list = None) -> bool:
     if words is None:
-        # TODO: demander les mots ici
-        pass
-
-    return False
+        word1=input("Le 1er Mot : ")
+        word2=input("Le 2eme Mot : ")
+        somme_word1=0
+        somme_word2=0
+        for chr in word1 :
+            somme_word1 += ord(chr)
+        for chr in word2 :
+            somme_word2 += ord(chr)
+        if somme_word1==somme_word2 :    
+            return True
+        else :
+            return False
 
 
 def contains_doubles(items: list) -> bool:
-    return False
+    return len(items) != len(set(items))
 
 
 def best_grades(student_grades: dict) -> dict:
     # TODO: Retourner un dictionnaire contenant le nom de l'étudiant ayant la meilleure moyenne ainsi que sa moyenne
-    return {}
+    meilleure_moyenne=0
+    etudiant=""
+    for element in student_grades :
+        moyenne = sum(student_grades[element])/len(student_grades[element])
+        if moyenne > meilleure_moyenne :
+            meilleure_moyenne= moyenne
+            etudiant=element
+    
+    return {etudiant : meilleure_moyenne}
 
 
 def frequence(sentence: str) -> dict:
     # TODO: Afficher les lettres les plus fréquentes
     #       Retourner le tableau de lettres
+    List={}
+    for element in sentence :
+        if sentence.count(element) >5 :
+            List[element]=sentence.count(element)
+        sorted_List=sorted(List.items(),key=lambda x:x[1],reverse=True)
 
-    return {}
+    return sorted_List
 
 
 def get_recipes():
     # TODO: Demander le nom d'une recette, puis ses ingredients et enregistrer dans une structure de données
-    pass
+    nom_recette=input("entrez le nom de recette")
+    ingredients= input("entrez les ingrendients separess d'espace")
+    ingredients=ingredients.split()
+    return {nom_recette: ingredients}
+
 
 
 def print_recipe(ingredients) -> None:
     # TODO: Demander le nom d'une recette, puis l'afficher si elle existe
-    pass
+    recette=input("entrez le nom de la recette")
+    if recette in ingredients :
+        print()
 
 
 def main() -> None:
