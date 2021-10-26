ded80004875691000e3686863075fcbcdbc9615f
Younes51-1 <77343582+Younes51-1@users.noreply.github.com>
2021-10-26 10:34:56 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -2,35 +2,18 @@
 # -*- coding: utf-8 -*-
 
 
-from typing import Counter, List
-
-
 def order(values: list = None) -> list:
-    values=list([])
-    for i in range(10) :
-        entrez=input("Entrez une valeur : ")
-        if entrez.isdigit()==True :
-            values.append(float(entrez))
-        else:
-            values.append(entrez)
-        values.sort()
-    return values
+    if values is None:
+        values = [input("Give a value: ") for _ in range(10)]
+
+    return sorted(values)  # We assume values are heterogeneous
 
 
 def anagrams(words: list = None) -> bool:
     if words is None:
-        somme_words1=0
-        somme_words2=0
-        anagram=False
-        words=[input("entrez le 1mot :"),input("entrez le 2eme mot :")]
-        for chr in words[0] :
-            somme_words1 += ord(chr)
-        for chr in words[1] :
-            somme_words2 += ord(chr)
-    if somme_words1==somme_words2 :
-        anagram=True
-    return anagram
+        words = [input("Enter a word: ") for i in range(2)]
 
+    return sorted(words[0]) == sorted(words[1])
 
 
 def contains_doubles(items: list) -> bool:
@@ -39,44 +22,43 @@ def contains_doubles(items: list) -> bool:
 
 def best_grades(student_grades: dict) -> dict:
     # TODO: Retourner un dictionnaire contenant le nom de l'étudiant ayant la meilleure moyenne ainsi que sa moyenne
-    meilleure_moyenne=0
-    etudiant=""
-    for element in student_grades :
-        moyenne = sum(student_grades[element])/len(student_grades[element])
-        if moyenne > meilleure_moyenne :
-            meilleure_moyenne= moyenne
-            etudiant=element
+    best_student = sorted(student_grades.items(), key=lambda grade: sum(grade[1]) / len(grade[1]), reverse=True)[0]
     
-    return {etudiant : meilleure_moyenne}
+    return {best_student[0]: sum(best_student[1]) / len(best_student[1])}
 
 
 def frequence(sentence: str) -> dict:
     # TODO: Afficher les lettres les plus fréquentes
     #       Retourner le tableau de lettres
-    liste_de_chiffre=[dict(Counter(sentence))]
-    dictionnaire={}
-    for i in liste_de_chiffre :
-        for cle in i :
-            if i[cle]>5 :
-                dictionnaire[cle]=i[cle]
-    dictionnaire=[sorted(dictionnaire.items(), key=lambda k:k[0],reverse=True)]
-    return dictionnaire
+
+    frequencies = {}
+    for letter in sentence:
+        if letter not in frequencies:
+            frequencies[letter] = 1
+        else:
+            frequencies[letter] += 1
+
+    
+    return frequencies
 
 
 def get_recipes():
     # TODO: Demander le nom d'une recette, puis ses ingredients et enregistrer dans une structure de données
-    nom_recette=input("entrez le nom de recette")
-    ingredients= input("entrez les ingrendients separess d'espace")
-    ingredients=ingredients.split()
-    return {nom_recette: ingredients}
+    recipe_name = input("Enter a recipe name: ")
+    ingredients = []
+
+    ingredient = input("Give an ingredient: ")
+    while ingredient:
+        ingredients.append(ingredient)
+        ingredient = input("Give an ingredient: ")
+
+    return {recipe_name: ingredients}
 
 
-
-def print_recipe(ingredients) -> None:
+def print_recipe(ingredients: dict) -> None:
     # TODO: Demander le nom d'une recette, puis l'afficher si elle existe
-    recette=input("entrez le nom de la recette")
-    if recette in ingredients :
-        print()
+    
+    print(ingredients.get(input("What recipe do you want to consult? "), ""))
 
 
 def main() -> None:
